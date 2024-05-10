d781a20f93ad95c6f57ce6540e0294b38cf66d7e
Nabeel <nabeel@explore.ai>
2024-05-10 10:44:01 +0200

    flask data def

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 import sqlite3
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-def query_database(number):
+@app.route('/data')
+def get_data():
+    number = request.args.get('number', default=0, type=int)  # Get 'number' from query parameters
     # Map numbers to specific column names
     columns = ['date', 'weekday', 'weekday_type',]
     selected_column = f"leave_days_{number}"
@@ -12,25 +14,10 @@ def query_database(number):
     cursor = conn.cursor()
     cursor.execute(f"SELECT date, weekday, weekday_type, {selected_column} FROM leave_days")
     data = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return data
+    if data:
+        return jsonify({'result': data})
+    else:
+        return jsonify({'result': 'No data found'})
 
-
-
-# @app.route('/data')
-# def get_data():
-#     number = request.args.get('number', default=0, type=int)  # Get 'number' from query parameters
-#     conn = sqlite3.connect('holiday&dates_db')
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT some_column FROM data_table WHERE condition_column = ?", (number,))
-#     data = cursor.fetchone()  # Or fetchall() if you expect multiple rows
-#     cursor.close()
-#     conn.close()
-#     if data:
-#         return jsonify({'result': data[0]})
-#     else:
-#         return jsonify({'result': 'No data found'})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
